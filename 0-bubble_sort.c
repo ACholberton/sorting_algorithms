f295798354e05e543e05165b25a43b325d59a4fc
ACholberton <1103@holbertonschool.com>
2020-01-31 00:30:14 +0000

    fixed betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,43 +9,46 @@
  */
 int _putchar(char c)
 {
-  return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the Bubble
+ *sort algorithm.
+ *@array: is the array of integers to be reordered
+ *@size: is the size of the array
+ *Return: Nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, temp, temp2;
-  int swapper;
+	size_t i, temp, temp2;
+	int swapper;
 
-  do
-    {
-      swapper = 0;
-      for (i = 0; i < size - 1; i++)
-	{
-	  if (array[i] > array[i + 1])
-	    {
-	      temp = array[i];
-	      array[i] = array[i + 1];
-	      array[i + 1] = temp;
-	      swapper = 1;
-
-	      for (temp = 0; temp < size; temp++)
+	do {
+		swapper = 0;
+		for (i = 0; i < size - 1; i++)
 		{
-		  temp2 = array[temp];
-		  if (temp2 > 9)
-		    _putchar(temp2 / 10 + '0');
-
-		  _putchar(temp2 % 10 + '0');
-		  if (array[temp + 1] != '\0')
-		    {
-		      _putchar(',');
-		      _putchar(' ');
-		    }
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapper = 1;
+				for (temp = 0; temp < size; temp++)
+				{
+					temp2 = array[temp];
+					if (temp2 > 9)
+						_putchar(temp2 / 10 + '0');
+					_putchar(temp2 % 10 + '0');
+					if (array[temp + 1] != '\0')
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				if (swapper == 1)
+					_putchar(10);
+			}
 		}
-
-	      if (swapper == 1)
-		_putchar(10);
-	    }
-	}
-    } while (swapper == 1);
+	} while (swapper == 1);
 }
