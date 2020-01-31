5cd4e1ec347a036f6a5e8390f3e5e2e41d74bb67
Alouie412 <alouie412@gmail.com>
2020-01-31 10:15:06 +0000

    Format change + Print array on swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,48 +1,28 @@
 #include "sort.h"
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
-
 void selection_sort(int *array, size_t size)
 {
-  size_t i, j, k, min_index;
-  int min_value, temp;
+	size_t i, j, min_index;
+	int min_value, swapper;
 
-  for (i = 0; i < size - 1; i++)
-    {
-      min_index = i;
-      min_value = array[i];
-      for (j = i + 1; j < size; j++)
+	for (i = 0; i < size - 1; i++)
 	{
-	  if (array[j] < min_value)
-	    {
-	      min_value = array[j];
-	      min_index = j;
-	    }
+		swapper = 0;
+		min_index = i;
+		min_value = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min_value)
+			{
+				min_value = array[j];
+				min_index = j;
+				swapper = 1;
+			}
+		}
+
+		array[min_index] = array[i];
+		array[i] = min_value;
+		if (swapper == 1)
+			print_array(array, size);
 	}
-      array[min_index] = array[i];
-      array[i] = min_value;
-      for (k = 0; k < size; k++)
-	{
-	  temp = array[k];
-	  if (temp > 9)
-	    _putchar(temp / 10 + '0');
-	  _putchar(temp % 10 + '0');
-	  if (array[k + 1] != '\0')
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	    }
-	}
-      _putchar('\n');
-    }
 }
