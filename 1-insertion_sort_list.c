a54b7c7adb095673053a678e9cf938d4e393280e
Alouie412 <alouie412@gmail.com>
2020-02-03 03:21:17 +0000

    Total rewrite: insertion sort logic

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,94 +1,45 @@
 #include "sort.h"
 
-/**
- * final_swap - Function that swaps the first two nodes
- * of the linked list if it needs swapping. After this
- * the insertion sort ends
- * @list: The almost completely sorted linked list
- * @current: Pointer to the node to be swapped
- * Return: Nothing
- */
-void final_swap(listint_t **list, listint_t *current)
+void node_swapper(listint_t *current)
 {
-	current = *list;
-	if (current->n > current->next->n)
-	{
-		list_swapper(current);
-		*list = (*list)->prev;
-		print_list(*list);
-	}
+	listint_t *temp = current->prev;
+
+	temp->next = current->next;
+	if (temp->next != NULL)
+		temp->next->prev = temp;
+
+	current->prev = temp->prev;
+	if (current->prev != NULL)
+		current->prev->next = current;
+
+	current->next = temp;
+	temp->prev = current;
 }
 
 /**
- * list_swapper - Function that moves the position of two nodes
- * by altering which nodes come before and after them
- * @current: The pointer to the current node for altering
+ * insertion_sort_list - Function that implements the
+ * insertion sort algorithm for linked lists
+ * @list: The unsorted doubly linked list
  * Return: Nothing
- * Description: Also checks whether the node before or after
- * is a NULL pointer, as attempting to connect to NULL will cause
- * a segmentation fault
- */
-void list_swapper(listint_t *current)
-{
-	listint_t *temp = NULL;
-
-	temp = current->next;
-	current->next = current->next->next;
-	temp->next = current;
-	if (current->next != NULL)
-		current->next->prev = current;
-
-	temp->prev = temp->prev->prev;
-	if (temp->prev != NULL)
-		temp->prev->next = temp;
-
-	current->prev = temp;
-}
-
-/**
- * insertion_sort_list - Function that sorts a doubly linked list
- * @list: The doubly linked list to be sorted
- * Return: Nothing
- * Description: Each node is checked and swapped accordingly. When
- * the pointer reaches the end of the list, another check is conducted
- * to check if there are still any unsorted nodes. If there are, reset
- * the pointer and conduct the swap
+ * Description: Each node is checked against the node before it.
+ * If the node is in the wrong spot, move the node backwards until
+ * it is in the correct spot. Has a time complexity of O(n)
+ * due to only needing 1 loop (and a bunch of if statements)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list, *list_scan = NULL, *head = *list;
+	listint_t *current = (*list)->next;
 
-	if (list == NULL)
-		return;
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (current != NULL)
 	{
-		if (current->next == NULL)
+		if (current->prev != NULL && current->n < current->prev->n)
 		{
-			list_scan = head;
-			while (list_scan != NULL)
-			{
-				if (list_scan->next == NULL)
-				{
-					final_swap(list, current);
-					return;
-				}
-				if (list_scan->n > list_scan->next->n)
-				{
-					current = *list, list_scan = *list;
-					break;
-				}
-				list_scan = list_scan->next;
-			}
-		}
-		else if (current->n > current->next->n)
-		{
-			list_swapper(current);
-			while ((*list)->prev != NULL)
-				if ((*list)->n > (*list)->prev->n)
-					*list = (*list)->prev;
+			node_swapper(current);
+			if ((*list)->prev != NULL && ((*list)->n > (*list)->prev->n))
+				*list = (*list)->prev;
 
 			print_list(*list);
 		}
