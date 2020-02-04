6b191aafe94d13385e1c9ac72c646436b95ef0eb
ACholberton <1103@holbertonschool.com>
2020-02-04 02:24:22 +0000

    fixed output issue

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -14,22 +14,23 @@ void shell_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	while (gap <= size)
+	while (gap <= size / 3)
 	{
 		gap = gap * 3 + 1;
 	}
-	for (gap = size / 2; gap > 0; gap /= 2)
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
 			tmp = array[i];
-			for (j = i; j >= gap && tmp < array[j - gap];
+			for (j = i; j >= gap && array[j - gap] > tmp;
 			     j -= gap)
 			{
 				array[j] = array[j - gap];
 			}
 			array[j] = tmp;
 		}
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
